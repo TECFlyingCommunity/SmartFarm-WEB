858c4db9fac15e2808258d34b08130fed85615d4
Riquelmy Silva <riquelmy.silva@grupoicts.com.br>
2022-05-22 19:25:58 -0300

    feat: base do layou do projeto

--- src/core/services/api.ts
+++ src/core/services/api.ts
@@ -1,13 +1,12 @@
-import axios from "axios"
-import environment from "../environments/enviroment"
+import axios from 'axios'
+import environment from '../environments/enviroment'
 
 const api = axios.create({
-    baseURL: `${ environment.backend_url }/api/v1`,
-    timeout: 5000,
-    headers: {
-        "Content-Type": "application/json"
-    }
+  baseURL: `${environment.backend_url}/api/v1`,
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 })
-
 
 export default api
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,103 +1,187 @@
-import React, { useEffect, useState } from "react";
-import { Layout, Menu } from "antd";
-import "antd/dist/antd.css";
-import "./estilo.css";
-import Rodape from "../../components/Footer/index";
-import { ISensor } from "../../core/interface"
-import CardChart from "../../components/CardChart";
-import api from '../../core/services/api'
+import React, { useEffect, useState } from 'react'
+import { Layout, Menu, Card, Row, Col } from 'antd'
+import 'antd/dist/antd.css'
+import './estilo.css'
 
-const { Header, Content, Footer } = Layout;
-const style = { background: "#0092ff", padding: "8px 0", margin: "1px 0 0 0" };
+import Rodape from '../../components/Footer/index'
+import { ISensor } from '../../core/interface'
+import CardChart from '../../components/CardChart'
+import api from '../../core/services/api'
+import { borderColor } from '@mui/system'
+
+const { Header, Content, Footer } = Layout
+const style = { background: '#0092ff', padding: '8px 0', margin: '1px 0 0 0' }
 
 interface IData {
-    date: string,
-    value: number,
-    name: string
+  date: string
+  value: number
+  name: string
 }
 
 const Dashboard: React.FC = () => {
+  const [data, setData] = useState<ISensor[]>([])
+  const [temperatureData, setTemperatureData] = useState<IData[]>([])
+  const [luminosityData, setLuminosityData] = useState<IData[]>([])
+  const [airHumidityData, setAirHumidityData] = useState<IData[]>([])
+  const [soilHumidityData, setSoilHumidityData] = useState<IData[]>([])
 
-    const [data, setData] = useState<ISensor[]>([]);
-    const [temperatureData, setTemperatureData] = useState<IData[]>([]);
-    const [luminosityData, setLuminosityData] = useState<IData[]>([]);
-    const [airHumidityData, setAirHumidityData] = useState<IData[]>([]);
-    const [soilHumidityData, setSoilHumidityData] = useState<IData[]>([]);
+  const formatData = (data: ISensor[]) => {
+    setTemperatureData([])
+    setLuminosityData([])
+    setAirHumidityData([])
+    setSoilHumidityData([])
 
+    data.forEach((element) => {
+      setTemperatureData((prev) => [
+        ...prev,
+        {
+          date: element.createdAt,
+          value: element.temperature,
+          name: 'Temperatura',
+        },
+      ])
+      setLuminosityData((prev) => [
+        ...prev,
+        {
+          date: element.createdAt,
+          value: element.luminosity,
+          name: 'Luminosidade',
+        },
+      ])
+      setAirHumidityData((prev) => [
+        ...prev,
+        {
+          date: element.createdAt,
+          value: element.airHumidity,
+          name: 'Umidade do Ar',
+        },
+      ])
+      setSoilHumidityData((prev) => [
+        ...prev,
+        {
+          date: element.createdAt,
+          value: element.soilHumidity,
+          name: 'Umidade do Solo',
+        },
+      ])
+    })
+  }
 
-    const formatData = (data: ISensor[]) => {
-        setTemperatureData([])
-        setLuminosityData([])
-        setAirHumidityData([])
-        setSoilHumidityData([])
+  const getData = (): void => {
+    api.get<ISensor[]>(`sensor`).then((response) => {
+      setData(response.data)
+    })
+  }
 
+  useEffect(() => {
+    getData()
+  }, [])
 
-        data.forEach(element => {
-            setTemperatureData(prev => [...prev, {
-                date: element.createdAt,
-                value: element.temperature,
-                name: "Temperatura"
-            }])
-            setLuminosityData(prev => [...prev, {
-                date: element.createdAt,
-                value: element.luminosity,
-                name: "Luminosidade"
-            }])
-            setAirHumidityData(prev => [...prev, {
-                date: element.createdAt,
-                value: element.airHumidity,
-                name: "Umidade do Ar"
-            }])
-            setSoilHumidityData(prev => [...prev, {
-                date: element.createdAt,
-                value: element.soilHumidity,
-                name: "Umidade do Solo"
-            }])
-        })
-    }
+  useEffect(() => {
+    formatData(data)
+  }, [data])
 
+  return (
+    <Layout className="container" style={{ background: '#2f3e46' }}>
+      <Content
+        className="site-layout"
+        style={{
+          padding: '0 50px',
+          marginTop: 64,
+          marginRight: 15,
+          marginLeft: 15,
+          background: '#84a98c',
 
-    const getData = (): void => {
-        api.get<ISensor[]>(`sensor`).then((response) => {
-            setData(response.data)
-        })
-    }
+          backgroundImage: 'url(/image.png)',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        <div className="site-card-wrapper">
+          <Card
+            style={{
+              background: '#cad2c5',
+              marginTop: 20,
+              borderColor: '#000000',
+              border: '2px solid red',
+            }}
+          >
+            <Row gutter={18}>
+              <Col xs={24} sm={24} md={12} lg={8} xl={12}>
+                <Card
+                  style={{
+                    textAlign: 'center',
+                    background: '#cad2c5',
+                    borderColor: '#cad2c5',
+                    border: '2px solid red',
+                  }}
+                >
+                  <CardChart
+                    data={temperatureData}
+                    title={'Temperatura'}
+                    loading={false}
+                  />
+                </Card>
+              </Col>
 
-    useEffect(() => {
-        getData()
-    }, [])
+              <Col xs={24} sm={24} md={12} lg={8} xl={12}>
+                <Card
+                  style={{
+                    textAlign: 'center',
+                    background: '#cad2c5',
+                    border: '2px solid red',
+                  }}
+                  bordered={false}
+                >
+                  <CardChart
+                    data={luminosityData}
+                    title={'Luminosidade'}
+                    loading={false}
+                  />
+                </Card>
+              </Col>
 
-    useEffect(() => {
-        formatData(data)
-    }, [data])
+              <Col xs={24} sm={24} md={12} lg={8} xl={12}>
+                <Card
+                  style={{
+                    textAlign: 'center',
+                    background: '#cad2c5',
+                    border: '2px solid red',
+                  }}
+                  bordered={false}
+                >
+                  <CardChart
+                    data={airHumidityData}
+                    title={'Umidade do Ar'}
+                    loading={false}
+                  />
+                </Card>
+              </Col>
 
-
-    return <Layout className="container">
-        <Header
-            style={ {
-                position: "fixed",
-                zIndex: 1,
-                width: "100%",
-            } }
-        >
-            <div className="logo"/>
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={ ["2"] }/>
-        </Header>
-        <Content
-            className="site-layout"
-            style={ {
-                padding: "0 50px",
-                marginTop: 64,
-            } }
-        >
-            <CardChart data={ temperatureData } title={ 'Temperatura' } loading={ false }/>
-            <CardChart data={ luminosityData } title={ 'Luminosidade' } loading={ false }/>
-            <CardChart data={ airHumidityData } title={ 'Umidade do Ar' } loading={ false }/>
-            <CardChart data={ soilHumidityData } title={ 'Umidade do Solo' } loading={ false }/>
-
-        </Content>
-        <Rodape/>
+              <Col xs={24} sm={24} md={12} lg={8} xl={12}>
+                <Card
+                  style={{
+                    textAlign: 'center',
+                    background: '#cad2c5',
+                    border: '2px solid red',
+                  }}
+                  bordered={false}
+                >
+                  <CardChart
+                    data={soilHumidityData}
+                    title={'Umidade do Solo'}
+                    loading={false}
+                  />
+                </Card>
+              </Col>
+            </Row>
+          </Card>
+        </div>
+      </Content>
+      <Footer>
+        <Rodape />
+      </Footer>
     </Layout>
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
